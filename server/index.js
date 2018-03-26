efc0f9b41dbf2a075e84e4985e6e0cf7716cd0de
webchao <chen@xiaochao.me>
2018-03-26 11:49:14 +0800

    koa-views+pug

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const Koa = require('koa');
-const app = new Koa();
+const views = require('koa-views'); //模板引擎
 const {
-    normal
-} = require('./tpl');
+    resolve
+} = require('path'); //路径
+const app = new Koa();
+
+app.use(views(resolve(__dirname, './views'), {
+    extension: 'pug'
+})) //只要是pug模板文件，都会被识别成并解析成html
 app.use(async (ctx, next) => {
-    ctx.type = 'text/html; charset=utf-8';
-    ctx.body = normal;
+    // 异步操作使用 await
+    await ctx.render('index', {
+        you: 'luke',
+        me: 'xiaochao'
+    })
+
 })
 app.listen(4455)
--- server/tpl/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    htmlTpl: require('./html'),
-    viewsTpl: require('./views')
-}
